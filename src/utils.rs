45234d2ea09dfaf6c45c57ae5e0f8d2541b23001
maxice8 <thinkabit.ukim@gmail.com>
2019-03-26 01:00:31 -0300

    9

--- src/utils.rs
+++ src/utils.rs
@@ -1,23 +1,44 @@
 use crate::errors::Error;
+use std::fmt::Write as FmtWrite;
 use std::fs::read_to_string;
 use std::fs::OpenOptions;
-use std::io::Write;
+use std::io::Write as IoWrite;
 use std::path::PathBuf;
+use std::process::Command;
 
 // Function to write to a fifo
-pub fn write_to_fifo(p: PathBuf, a: String) -> Result<(), Error> {
+pub fn write_to_fifo(p: PathBuf, a: String) -> Result<String, Error> {
     // Try to open the fifo
-    let fifo = match OpenOptions::new().read(true).write(true).open(&p) {
+    let mut fifo = match OpenOptions::new().read(true).write(true).open(&p) {
         Ok(p) => p,
         Err(e) => return Err(Error::Open(p.into_os_string().into_string().unwrap(), e)),
     };
 
-    println!("Opened fifo for writing");
+    // NOTE: find a way to do it in pure rust
+    let child = Command::new("/bin/head")
+        .arg("-c")
+        .arg("1")
+        .arg(&p)
+        .spawn()
+        .expect("Failed to attach to fifo!");
 
     match write!(fifo, "{}", a) {
-        Ok(_) => Ok(()),
+        Ok(_) => (),
         Err(e) => return Err(Error::Write(p.into_os_string().into_string().unwrap(), e)),
     }
+
+    let stdout: Vec<u8> = match child.wait_with_output() {
+        Ok(f) => f.stdout,
+        Err(e) => return Err(Error::Read(p.into_os_string().into_string().unwrap(), e)),
+    };
+
+    let mut out = String::new();
+
+    for n in stdout {
+        let _ = write!(&mut out, "{}", n);
+    }
+
+    Ok(out)
 }
 
 // Open a file for reading and write to it, return an error if unable to
